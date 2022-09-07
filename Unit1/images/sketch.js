f2e7721999588fc5cc050c403c19654256d49686
McWilliams45 <90366813+McWilliams45@users.noreply.github.com>
2022-09-06 19:17:16 -0500

    Assignment

---
+++ Unit1/images/sketch.js
@@ -0,0 +1,17 @@
+let i1, i2, i3 ;
+
+function setup() {
+  createCanvas(500, 500);
+  i1 = loadImage ("assets/pizza.jpeg") ;
+  i2 = loadImage ("assets/whiteclaw.jpeg") ;
+  i3 = loadImage ("assets/film.jpeg") ;
+  imageMode(CENTER);
+}
+
+function draw() {
+background("grey"); 
+Image(i1, width/2, height /2-120 , 100, 100) ; 
+Image(i2, width/2, height /2 , 100, 100) ; 
+Image(i3, width/2, height /2 + 120, 100, 100) ; 
+
+}
