dfe68270978880ecee1cc41c5aba809db92e6f60
McWilliams45 <90366813+McWilliams45@users.noreply.github.com>
2022-09-06 19:29:28 -0500

    image

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -2,16 +2,16 @@ let i1, i2, i3 ;
 
 function setup() {
   createCanvas(500, 500);
-  i1 = loadImage ("assets/pizza.jpeg") ;
-  i2 = loadImage ("assets/whiteclaw.jpeg") ;
-  i3 = loadImage ("assets/film.jpeg") ;
+  i1 = loadImage("assets/pizza.jpeg") ;
+  i2 = loadImage("assets/whiteclaw.jpeg") ;
+  i3 = loadImage("assets/film.jpeg") ;
   imageMode(CENTER);
 }
 
 function draw() {
 background("grey"); 
-image(i1, width/2, height /2-120 , 100, 100) ; 
-image(i2, width/2, height /2 , 100, 100) ; 
-image(i3, width/2, height /2 + 120, 100, 100) ; 
+image(i1, width/ 2, height / 2-120 , 100, 100) ; 
+image(i2, width/ 2, height / 2 , 100, 100) ; 
+image(i3, width/ 2, height / 2 + 120, 100, 100) ; 
 
 }
