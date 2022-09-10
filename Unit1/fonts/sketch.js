c95d13958ed29703b7cb383336e0d16aa7ff4f45
McWilliams45 <90366813+McWilliams45@users.noreply.github.com>
2022-09-10 14:06:16 -0500

    sound

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,7 +1,19 @@
+let f1, f2 ;
+
+
+
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("assets/happy.otf") ;
+  f2 = loadFont("assets/rift.otf") ;
+  textAlign(CENTER); 
 }
 
 function draw() {
-
+ background("black");
+ fill("white");
+textFont(f1,48);
+text("hello there!",width/ 2, 100);
+textFont(f2,48);
+text("I am Obi Wan!",width/ 2, 100);
 }
---
+++ Unit1/sounds/sketch.js
@@ -0,0 +1,18 @@
+let downtown;
+
+ function preload() {
+  downtown = loadSound("downtown.mp3");  
+}
+
+
+function setup() {
+  createCanvas(500, 500);
+  textAlign(CENTER); 
+  downtown.play() ; 
+}
+
+function draw() {
+ background("black");
+ fill("white");
+
+}
